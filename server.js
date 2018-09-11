488179b0d1f1d03cd5e7d39b6bddbcf8d60bb222
Jayce Azua <jayce.azua@gmail.com>
2018-09-11 15:16:15 -0700

    help....

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
 const app = express();
 const port =  process.env.PORT || 3000;
+
+// MongoDB Connection
+require('./mongo-database-connection');
 
 const theaters = require('./controllers/theaters');
 
 app.use('/theaters', theaters);
 
 module.exports = app.listen(port, () => {
-  console.log('Alive!')
+  console.log('Action!')
 });
