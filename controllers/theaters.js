4e0acae70b24892101ca7d10e3105055d782f7da
Jayce Azua <jayce.azua@gmail.com>
2018-09-11 15:01:35 -0700

    setting up routes

---
+++ controllers/theaters.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// CREATE - post a new
+router.post('');
+// READ
+router.get('/:id')
+// UPDATE - update
+router.put('/:id')
+// DELETE
+router.delete('/:id')
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const router = express.Router();
+
 const app = express();
 const port =  process.env.PORT || 3000;
 
-app.get('/', (req, res, next) => {
-  res.send('madafaka')
-});
+const theaters = require('./controllers/theaters');
+
+app.use('/theaters', theaters);
 
 module.exports = app.listen(port, () => {
   console.log('Alive!')
-})
+});
