de44512e3f47205fb62549e65fae38dc435174ba
Jayce Azua <jayce.azua@gmail.com>
2018-10-02 10:44:19 -0700

    this is some fucking dumb shit

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-require('dotenv').config();
 const express = require('express');
-const router = express.Router();
+const mongo = require('mongodb');
 const app = express();
 const port =  process.env.PORT || 3000;
 
-// MongoDB Connection
-require('./mongo-database-connection');
 
-const theaters = require('./controllers/theaters');
-router.use('/theaters', theaters);
 
-module.exports = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Action! Server is on at port: ${port}`)
 });
